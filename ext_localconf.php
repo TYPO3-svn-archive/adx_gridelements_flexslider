5c9b4ae7721112fe61f2a037dbab7e18dd6ed54c
adgrafik <adgrafik@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-13 10:24:17 +0000

    [TASK] Changes including of JavaScript to t3jquery. [TASK] Moved JavaScript to footer.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/adx_gridelements_flexslider/trunk@71166 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
-// Register stdWrap hook for includeFrontEndResources. Using "adx*" key to prevent colliding of other adx-extensions using the same hook.
-$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['stdWrap']['adxIncludeFrontEndResources'] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/StdWrap.php:&Tx_AdxGridelementsFlexslider_Hooks_StdWrap';
-
 // Include TypoScript configuration.
 t3lib_extMgm::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/Page.ts">');
 
